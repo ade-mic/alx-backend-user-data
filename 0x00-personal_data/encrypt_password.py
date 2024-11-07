b2299fe68270948507e7cc481dc3b435dd56dcc2
ade-mic <ademic.aina@gmail.com>
2024-11-07 20:50:35 +0100

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,11 +3,11 @@
 a hash_password function that expects
 one string argument name password and
 returns a salted, hashed password, which is a byte string.
- """
+"""
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """
     a hash_password function that expects
     one string argument name password and
