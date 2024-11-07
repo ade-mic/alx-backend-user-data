61598941f394e6235baef44824dfd9eea4d302c1
ade-mic <ademic.aina@gmail.com>
2024-11-07 20:17:47 +0100

    connectToDB

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -107,7 +107,7 @@ def get_db() -> connection.MySQLConnection:
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     database = os.getenv('PERSONAL_DATA_DB_NAME', 'holberton')
-    return mysql.connector.connect(
+    return connection.MySQLConnection(
         user=username,
         password=password,
         host=host,
