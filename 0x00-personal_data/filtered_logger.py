50a355864a532b15611ee638444c7428492b6735
ade-mic <ademic.aina@gmail.com>
2024-11-07 18:39:41 +0100

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,7 +29,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields:List[str]):
+    def __init__(self, fields: List[str]):
         """
         an initiator class for the
         RedactingFormatter class
