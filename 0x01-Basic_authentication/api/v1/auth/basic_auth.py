c5b2934b149d282c45972480116b21f48281438e
ade-mic <ademic.aina@gmail.com>
2024-11-12 09:45:50 +0100

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,17 @@ class BasicAuth(Auth):
     class BasicAuth that inherits from Auth.
     For the moment this class will be empty.
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str
+                                            ) -> str:
+        """
+         returns the Base64 part of the Authorization
+         header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header.split(' ')[1]
