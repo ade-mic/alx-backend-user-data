fa7da016f7191b8ae9981cabfbac9bae5f6c270f
ade-mic <ademic.aina@gmail.com>
2024-11-12 10:03:08 +0100

    . Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ class BasicAuth that inherits from Auth.
 For the moment this class will be empty.
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,3 +26,25 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                            base64_authorization_header: str
+                                            ) -> str:
+        """
+        Returns the decoded
+        value of a Base64 string base64_authorization_header
+        Args:
+            base64_authorization_header (str)
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(
+                base64_authorization_header
+            )
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except(base64.binascii.Error, UnicodeDecodeError):
+            return None
