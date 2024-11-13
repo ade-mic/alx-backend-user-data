b5ad18b20441c104a1bf5ea4c1bcda22ebb9c7cf
ade-mic <ademic.aina@gmail.com>
2024-11-13 18:46:56 +0100

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+ class SessionAuth that inherits from Auth.
+ For the moment this class will be empty.
+ It’s the first step for creating a new authentication mechanism:
+"""
+from auth import Auth
+
+
+class SessionAuth(Auth):
+     """
+        class SessionAuth that inherits from Auth.
+        For the moment this class will be empty.
+        It’s the first step for creating a new authentication mechanism:
+    """
