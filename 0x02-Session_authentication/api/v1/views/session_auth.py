d687b44394a2d8d89767ca4c4de42a3da023a168
ade-mic <ademic.aina@gmail.com>
2024-11-14 14:33:18 +0100

    7. New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -11,20 +11,20 @@ from models.user import User
 def login():
     """ POST  /api/v1/auth_session/login
     Return:
-      - 
+      - the dictionary representation of the User
     """
     email = request.form.get('email')
     password = request.form.get('password')
     if email is None or email == '':
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if password is None or password == '':
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     users = User.search({'email': email})
     if not users or len(users) == 0:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     user = users[0]
     if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password" }), 401
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
     response = jsonify(user.to_json())
