281d426f913df1bac826a45f2c800b8fa888fa3a
ade-mic <ademic.aina@gmail.com>
2024-11-14 08:31:10 +0100

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
  For the moment this class will be empty.
  It’s the first step for creating a new authentication mechanism:
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -13,4 +14,27 @@ class SessionAuth(Auth):
         For the moment this class will be empty.
         It’s the first step for creating a new authentication mechanism:
     """
-    pass
+    def __init__(self) -> None:
+        """Initialize SessionAuth"""
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id:
+        Return None if user_id is None
+        Return None if user_id is not a string
+        Otherwise:
+            Generate a Session ID using uuid module and uuid4() like id in Base
+            Use this Session ID as key of the dictionary
+            user_id_by_session_id - the value for this key must be user_id
+        Return the Session ID
+        The same user_id can have multiple Session ID - indeed,
+        the user_id is the value in the dictionary user_id_by_session_id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id = {session_id: user_id}
+        return session_id
