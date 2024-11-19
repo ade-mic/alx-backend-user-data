e275fd5e7e46870214aa4b26b3cab792f9467f86
ade-mic <ademic.aina@gmail.com>
2024-11-19 09:43:39 +0100

    user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -21,6 +21,19 @@ Base = declarative_base()
 
 
 class User(Base):
+    """
+    In this module we create a SQLAlchemy model named
+    User for a database table named users
+    (by using the mapping declaration of SQLAlchemy).
+
+    The model will have the following attributes:
+    class: User
+    Args:
+        id(int): the integer primary key
+        email(str), a non-nullable string
+    hashed_password(str), a non-nullable string
+    session_id(str): a nullable string
+"""
     __tablename__ = 'users'
     id: int = Column(Integer, primary_key=True)
     email: str = Column(String(250), nullable=False)
